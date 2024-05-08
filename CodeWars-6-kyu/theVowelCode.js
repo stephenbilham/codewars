78c44a1b962b6d0f453940e4a3f612146b3ff2ff
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-05-08 14:08:06 -0700

    the vowel code

--- CodeWars-6-kyu/theVowelCode.js
+++ CodeWars-6-kyu/theVowelCode.js
@@ -1,37 +1,63 @@
+// function encode(string) {
+// 	// Replace all lowercase vowels with corresponding numbers
+// 	return string.replace(/[aeiou]/g, (match) => {
+// 		switch (match) {
+// 			case "a":
+// 				return "1";
+// 			case "e":
+// 				return "2";
+// 			case "i":
+// 				return "3";
+// 			case "o":
+// 				return "4";
+// 			case "u":
+// 				return "5";
+// 		}
+// 	});
+// }
+
+// function decode(string) {
+// 	// Replace all numbers with corresponding vowels
+// 	return string.replace(/[1-5]/g, (match) => {
+// 		switch (match) {
+// 			case "1":
+// 				return "a";
+// 			case "2":
+// 				return "e";
+// 			case "3":
+// 				return "i";
+// 			case "4":
+// 				return "o";
+// 			case "5":
+// 				return "u";
+// 		}
+// 	});
+// }
+
+const table = ["a", "e", "i", "o", "u"];
+
 function encode(string) {
-	// Replace all lowercase vowels with corresponding numbers
-	return string.replace(/[aeiou]/g, (match) => {
-		switch (match) {
-			case "a":
-				return "1";
-			case "e":
-				return "2";
-			case "i":
-				return "3";
-			case "o":
-				return "4";
-			case "u":
-				return "5";
-		}
-	});
+	return string
+		.split("")
+		.map((letter) => {
+			let index = table.indexOf(letter);
+			if (index === -1) {
+				return letter;
+			} else {
+				return index + 1;
+			}
+		})
+		.join("");
 }
 
 function decode(string) {
-	// Replace all numbers with corresponding vowels
-	return string.replace(/[1-5]/g, (match) => {
-		switch (match) {
-			case "1":
-				return "a";
-			case "2":
-				return "e";
-			case "3":
-				return "i";
-			case "4":
-				return "o";
-			case "5":
-				return "u";
-		}
-	});
+	return string
+		.split("")
+		.map((char) => {
+			const num = parseInt(char);
+			return isNaN(num) ? char : table[num - 1];
+		})
+		.join("");
 }
 
 // Example usage:
