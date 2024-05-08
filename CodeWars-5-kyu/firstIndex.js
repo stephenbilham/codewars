f24f91d16d9d076f006dcc9e78b418b12ec9852a
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-05-08 15:06:04 -0700

    first occur

---
+++ CodeWars-5-kyu/firstIndex.js
@@ -0,0 +1,45 @@
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+	let pointer = 0;
+	for (let i = 0; i < haystack.length; i++) {
+		if (haystack[i] !== needle[pointer]) {
+			i = i - pointer;
+			pointer = 0;
+		} else if (pointer === needle.length - 1) {
+			return i - pointer;
+		} else {
+			pointer++;
+		}
+	}
+	return -1;
+};
+
+console.log(strStr("sadbutsad", "sad"));
